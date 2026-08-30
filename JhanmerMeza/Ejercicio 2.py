02076a404308a0a9554c51ae86ada41dede8b999
MezaJhanmer1 <mezajhanmer5@gmail.com>
2026-08-30 16:24:46 -0500

    Add files via upload

---
+++ JhanmerMeza/Ejercicio 2.py
@@ -0,0 +1,10 @@
+numero1 = float(input("Ingresa el primer numero: "))
+numero2 = float(input("Ingresa el segundo numero: "))
+
+if numero1 > numero2:
+    print(f"El {numero1} es mayor ")
+elif numero2 > numero1:
+    print(f"El {numero2}es mayor")
+else:
+    print("Los numero son iguales")
+
